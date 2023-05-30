3c62f185925a0e693e4d88b8ebd90484c3904f82
zhp <Zhou_Hepeng@qq.com>
2023-05-30 13:46:32 +0800

    pref:修改文件名

---
+++ os-demo/admin/src/App.vue
@@ -0,0 +1,149 @@
+<template>
+  <div id="app">
+    <header class="public-header">
+      <a class="logo"> </a>
+      <!-- <div class="user">欢迎，管理员</div> -->
+    </header>
+    <div class="container">
+      <div class="sidebar">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </div>
+      <div class="main">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item label="审批人">
+            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域">
+            <el-select v-model="formInline.region" placeholder="活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">查询</el-button>
+          </el-form-item>
+        </el-form>
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column prop="date" label="日期" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+      formInline: {
+        user: '',
+        region: '',
+      },
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #f5f5f5;
+}
+.public-header {
+  display: flex;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  padding: 0 30px;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+}
+.logo{
+  width:150px;
+  height:60px;
+  background-color:rgba(71, 121, 236, 0.652)
+}
+.container {
+  display: flex;
+}
+.sidebar {
+  width: 200px;
+  background-color:#fff;
+}
+.main {
+  margin: 20px;
+  padding: 20px;
+  flex: 1;
+  border-radius: 10px;
+  background-color:#fff;
+}
+</style>
---
+++ os-demo/mobile/src/App.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <header>Header</header>
+    
+    <section>
+      <aside>
+        <img src="/m-img/logo.png" alt="">
+      </aside>  
+      <a href="123">这个是链接</a>
+      <main>
+        <div class="container">
+          <div class="row">
+            <div class="col-4" v-for="item in 25" :key="item">
+            <div class="box"><img :src="`https://dummyimage.com/150x120/9d2315/fff`" alt=""></div>            
+            </div>            
+          </div>          
+        <div class="container">        
+          <div class="row">
+            <div class="col-6 col-sm-4 col-md-3 col-lg-2" v-for="item in 25" :key="item">
+              <div class="box"><img :src="`https://dummyimage.com/150x120/9d2315/fff`" alt=""></div>          
+            </div>                          
+          </div>        
+        </div> 
+          <div class="container">       
+          <div class="row">             
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="item in 25" :key="item">
+              <div class="box"><img :src="`https://dummyimage.com/150x120/9d2315/fff`" alt=""></div>
+            </div>
+          </div>
+        </div>  
+        </div>  
+      </main> 
+    </section>
+
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  } 
+}
+</script>
+
+<style>
+/*
+*  css reset
+*/
+*{
+  margin:0;
+  padding:0;
+}
+
+ul,li{
+  list-style: none;
+}
+a{
+  color:#3d3d3d;
+  text-decoration: none; /**去掉a链接的下划线 */
+}
+
+
+
+header, footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  padding: 20px 0;  
+}
+
+aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  padding: 100px 0;  
+}   
+
+main {
+  background-color: #E9EEF3;
+  color: #333; 
+  padding: 60px 0;  
+}
+
+.container {
+  width: 100%; 
+  margin: 0 auto;
+  padding: 0 20px; 
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.col-4 {
+  width: 33%;
+}
+
+.col-6 {
+  width: 50%; 
+}
+
+.col-12 {
+  width: 100%;  
+}
+
+@media (min-width: 576px) { 
+  .col-sm-4 {
+    width: 33%;   
+  }
+  
+  .col-sm-6 {
+    width: 50%;   
+  }  
+}
+
+@media (min-width: 768px) { 
+  .col-md-3 {
+    width: 25%;
+  }
+  
+  .col-md-4 {
+    width: 33%; 
+  } 
+}
+
+@media (min-width: 992px) { 
+  .col-lg-2 {
+    width: 16.66%;
+  }
+  
+  .col-lg-3 {
+    width: 25%;   
+  } 
+}
+
+.box {
+  border: 1px solid #999;
+  padding: 20px; 
+}
+</style>
